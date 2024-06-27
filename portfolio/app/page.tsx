9d8e9c48a98f64d13680df3a4f1f69948003ffcd
abolfazlnik <abolfazlnikk@gmail.com>
2024-06-27 14:47:46 +0330

    fix smooth scroll and clean import files

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,28 +1,23 @@
 "use client"
 import { useRef } from "react"
 import Image from "next/image"
-import DynamicScrollTriggerComponent from "@/app/components/DynamicScrollTriggerComponent"
-import ProjectsScrollSection from "@/app/components/ProjectsScrollSection"
-import About from "@/app/components/About"
-import MarqueeScroll from "@/app/components/MarqueeScroll"
-import Skills from "@/app/components/skills"
-import Experience from "@/app/components/experience"
 import { nikName } from "@/public/images"
-import gsap from "gsap"
-import { ScrollTrigger } from "gsap/ScrollTrigger"
-import Header from "./shared/components/header"
-import BallButton from "./shared/components/bull-button"
-import { Scrollbar } from "smooth-scrollbar-react"
-import type { Scrollbar as BaseScrollbar } from "smooth-scrollbar/scrollbar"
-gsap.registerPlugin(ScrollTrigger)
+import { BallButton, Header } from "./shared/components"
+import {
+  About,
+  DynamicScrollTriggerComponent,
+  Experience,
+  MarqueeScroll,
+  ProjectsScrollSection,
+  Skills,
+} from "./components"
 
 export default function Home() {
-  const scrollbar = useRef<BaseScrollbar | null>(null)
   const aboutRef = useRef(null)
   const projectRef = useRef(null)
 
   return (
-    <Scrollbar ref={scrollbar}>
+    <>
       <div className="h-screen">
         <Header />
         <Image className="w-full h-[85%] select-none" src={nikName} alt="nik" />
@@ -60,6 +55,6 @@ export default function Home() {
       <Experience />
 
       <BallButton />
-    </Scrollbar>
+    </>
   )
 }
