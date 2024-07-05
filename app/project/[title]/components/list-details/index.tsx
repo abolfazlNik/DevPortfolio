03bfa9fca3313d2e2da6f642fb06fefe601198b0
System Administrator <root@MacBook-Pro.local>
2024-07-05 13:07:50 +0330

    fix style project page

--- app/project/[title]/components/list-details/index.tsx
+++ app/project/[title]/components/list-details/index.tsx
@@ -5,7 +5,7 @@ const ListDetails = ({ content }: { content: string[] }) => (
     <h3 className="sm:text-3xl text-2xl">{content[0]}</h3>
     <ul className="sm:text-2xl text-xl font-thin">
       {content[1].split("\n").map((line, index) => (
-        <li className="leading-10 list-disc" key={index}>
+        <li className="leading-10 list-disc sm:ms-6 ms-4" key={index}>
           {line}
           <br />
         </li>
